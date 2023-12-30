d4a8cf4c053ee564d3afb46911e54fac2e5fa207
kjj8781 <kjj8781@gmail.com>
2023-12-30 13:55:47 -0600

    everything working v1

--- scripts/materialId.py
+++ scripts/materialId.py
@@ -1,5 +1,6 @@
 import random
 import colorsys 
+import fnmatch
 
 import maya.OpenMayaUI as omui
 import maya.cmds as cmds 
@@ -28,51 +29,128 @@ class ResizableButton(QtWidgets.QPushButton):
         # no layout set, return default sizeHint
         return super().sizeHint()
 
+# Main UI class and functions
 def main_window():
     window_int = omui.MQtUtil.mainWindow()
     return wrapInstance(int(window_int), QtWidgets.QWidget)
 
-# UI Class
 class MaterialUI(QtWidgets.QDialog):
     def __init__(self, parent=main_window()):
         super(MaterialUI, self).__init__(parent)
         self.setWindowFlags(QtCore.Qt.Window)
         self.setObjectName('MaterialIdUI')
         self.setWindowTitle('Material ID Generator')
-        self.setGeometry(1450, 350, 400, 400)
+        self.resize(400, 400)
+
+        self.settings = QtCore.QSettings('KYBER', 'MaterialIDGenerator')
+        max_col = self.load_column_settings()
 
         self.create_shaders()
         self.check_window_exists()
-        self.create_ui()
+        self.create_ui(max_col)
 
-    # prevent window duplication
+    # Shader creation methods
+    def rand_color(self):
+        # comment out seed line to randomize palette on every run 
+        # or change the seed to get different color palettes
+        random.seed(80)
+
+        # use HSV to get distinct colors
+        color_list = [i/len(mat_list) for i in range(len(mat_list))]
+        hsv_colors = [(random.choice(color_list), 1, random.random()) 
+                      for i in range(len(mat_list))]
+
+        # convert HSV to RGB
+        rgb_colors = [colorsys.hsv_to_rgb(*hsv) for hsv in hsv_colors]
+        return rgb_colors
+
+    def populate_shader_dict(self):
+        color_list = self.rand_color()
+        shader_dict = {mat: color for mat, color in zip(mat_list, color_list)}
+        return shader_dict
+
+    def create_shaders(self):
+        shader_dict = self.populate_shader_dict()
+        for name, color in shader_dict.items():
+            mat = cmds.shadingNode('lambert', asShader=True, n=name, 
+                                   shared=True, ss=True)
+            cmds.setAttr(name + '.color', *color)
+
+            # Create shader groups only if they do not exist
+            sg_list = cmds.ls(type='shadingEngine')
+            sg_filter = fnmatch.filter(sg_list, f'{name}*')
+            if len(sg_filter) == 0:
+                sg = cmds.sets(n=name + 'SG', em=True, r=True, nss=True)
+                cmds.connectAttr(mat + '.outColor', sg + '.surfaceShader', f=True)
+            
+    # Shader manipulation methods
+    def apply_mat(self):
+        clicked_button = self.sender()
+        mat_name = clicked_button.findChild(QtWidgets.QLabel).text()
+        cmds.hyperShade(a=mat_name)
+
+    def reset_mat(self):
+        cmds.hyperShade(a='lambert1')
+    
+    # Custom column methods
+    def get_column_input(self):
+        # Close main ui in order to refresh button layout
+        if QtWidgets.QApplication.instance():
+            for window in (QtWidgets.QApplication.allWindows()):
+                if 'MaterialIdUI' in window.objectName():
+                    window.close()
+        
+        settings = self.settings
+        col_input, ok = QtWidgets.QInputDialog.getInt(
+            self, 'Change Button Columns', 'Number of Button Columns:', settings.value('numOfColumns'), 1, 20, 1
+        )
+        
+        if ok:
+            settings.setValue('numOfColumns', col_input)
+
+            # Reopen main ui with new settings
+            ui = MaterialUI()
+            ui.show()
+
+    def load_column_settings(self):
+        settings = self.settings
+        if settings.contains('numOfColumns'):
+            max_col = settings.value('numOfColumns')
+        else:
+            max_col = 2 # default numOfColumns
+        return max_col
+
+    # UI window methods
+    # Prevent window duplication
     def check_window_exists(self):
         if QtWidgets.QApplication.instance():
             for window in (QtWidgets.QApplication.allWindows()):
                 if 'MaterialIdUI' in window.objectName():
                     window.close()
 
-    def create_shaders(self):
-        shader_dict = self.populate_shader_dict()
-        for name, color in shader_dict.items():
-            cmds.shadingNode('lambert', asShader=True, n=name, shared=True, ss=True)
-            cmds.setAttr(name + '.color', *color)
-
-    def create_ui(self):
+    def create_ui(self, max_col):
         main_layout = QtWidgets.QVBoxLayout()
-
+        
+        # Menu bar
+        menu_bar = QtWidgets.QMenuBar()
+        options_menu = menu_bar.addMenu("Options")
+        change_num_col = QtWidgets.QAction('Change Number of Columns', menu_bar)
+        change_num_col.triggered.connect(self.get_column_input)
+        options_menu.addAction(change_num_col)
+        
         # Independent tools
-        tool_hbox = QtWidgets.QHBoxLayout()
-        remove_btn = QtWidgets.QPushButton('Remove Material')
+        func_hbox = QtWidgets.QHBoxLayout()
+        reset_btn = QtWidgets.QPushButton('Reset Material')
+        reset_btn.clicked.connect(self.reset_mat)
 
-        tool_hbox.addWidget(remove_btn)
+        func_hbox.addWidget(reset_btn)
 
-        # The shader port requires a window with a layout
+        # Shader scroll area and widgets
+        # The maya port requires a window with a layout
         tempwin = cmds.window()
         cmds.columnLayout()
         
-        # Initialize shader area layouts and variables
-        grid_layout = QtWidgets.QGridLayout()
+        self.grid_layout = QtWidgets.QGridLayout()
         scroll_area = QtWidgets.QScrollArea()
         shader_widget = QtWidgets.QWidget()
         row = 0
@@ -80,7 +158,7 @@ class MaterialUI(QtWidgets.QDialog):
 
         # Create all shader buttons
         for mat in mat_list:
-            if col == 2:
+            if col == max_col:
                 col = 0
                 row += 1
 
@@ -92,50 +170,36 @@ class MaterialUI(QtWidgets.QDialog):
             widget = QtWidgets.QWidget()
             qport.setParent(widget)
 
-            # Initialize button with horizontal layout and add port to btn
+            # Initialize button and label
             shader_btn = ResizableButton()
+            shader_btn.clicked.connect(self.apply_mat)
+            shader_label = QtWidgets.QLabel(mat)
+
+            # Add horizontal layout and port to button
             shader_hbox = QtWidgets.QHBoxLayout()
             widget.setLayout(shader_hbox)
             shader_btn.setLayout(shader_hbox)
 
-            label = QtWidgets.QLabel(mat)
-
             shader_hbox.addWidget(qport, alignment=QtCore.Qt.AlignLeft)
-            shader_hbox.addWidget(label, alignment=QtCore.Qt.AlignCenter)
+            shader_hbox.addWidget(shader_label, alignment=QtCore.Qt.AlignCenter)
 
-            grid_layout.addWidget(shader_btn, row, col)
+            self.grid_layout.addWidget(shader_btn, row, col)
 
             col += 1
 
         # Delete temp window after ports created
         cmds.deleteUI(tempwin)
 
-        # Set main layouts
-        shader_widget.setLayout(grid_layout)
+        # Set shader area main layouts
+        shader_widget.setLayout(self.grid_layout)
         scroll_area.setWidgetResizable(True)
         scroll_area.setWidget(shader_widget)
-
-        main_layout.addLayout(tool_hbox)
+        
+        main_layout.addWidget(menu_bar)
+        main_layout.addLayout(func_hbox)
         main_layout.addWidget(scroll_area)
         self.setLayout(main_layout)
     
-    def rand_color(self):
-        # comment out seed line to randomize palette on every run or
-        # change the seed to get different color palettes
-        random.seed(80)
-
-        # use HSV to get distinct colors
-        color_list = [i/len(mat_list) for i in range(len(mat_list))]
-        hsv_colors = [(random.choice(color_list), 1, random.random()) for i in range(len(mat_list))]
-
-        # convert HSV to RGB
-        rgb_colors = [colorsys.hsv_to_rgb(*hsv) for hsv in hsv_colors]
-        return rgb_colors
-
-    def populate_shader_dict(self):
-        color_list = self.rand_color()
-        shader_dict = {mat: color for mat, color in zip(mat_list, color_list)}
-        return shader_dict
-
-ui = MaterialUI()
-ui.show()
+if __name__ == "__main__":
+    ui = MaterialUI()
+    ui.show()
